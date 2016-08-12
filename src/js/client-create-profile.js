a3bb45e19ebc892a588b5145be7a5b89ec18fa81
Kirill Mangutov <kmangutov@gmail.com>
2016-08-11 17:40:02 -0700

    finish client create profile, begin query

---
+++ src/js/client-create-profile.js
@@ -0,0 +1,47 @@
+
+var userService = LexUserService("clients")
+
+var getForm = function() {
+
+	var inputs = [
+		"firstName",
+		"lastName",
+		"phone",
+		"primaryAddress",
+		"secondaryAddress",
+		"occupation",
+		"employer",
+		"income",
+		"updates"
+	];
+
+	var data = {};
+
+	inputs.forEach(function(input) {
+		var val = $("#" + input + "Input").val();
+		data[input] = val;
+	});
+
+	return data;
+}
+
+$(document).ready(function() {
+
+	$("#signup").click(function() {
+
+
+		//TODO: VALIDATE FORM
+
+		var success = function() {
+			alert("Success");
+		};
+
+		var fail = function() {
+			alert("Fail");
+		}
+
+		var userId = _getLoggedInUserId();
+		userService.put(userId, getForm(), success, fail);
+
+	});
+});
