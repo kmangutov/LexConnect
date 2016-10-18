6fa507449cd6c78267439fd1ad8f6015b1352a56
Kirill Mangutov <kmangutov@gmail.com>
2016-10-18 00:01:00 -0700

    fuck maybe fixed client multi submit

--- js/client-dashboard.js
+++ js/client-dashboard.js
@@ -4,6 +4,15 @@ var clientsService = LexConnectService("clients")
 var attorneysService = LexConnectService("attorneys")
 
 
+var ifClientConnectedTo = function(query, attorneyId, next, end) {
+	query.connectedAttorneys.find(function(connectedAttorney) {
+		if(connectedAttorney.attorneyId === attorneyId) {
+			next();
+			return;
+		}
+		end();
+	});
+}
 
 
 var joinedQueries = [];
@@ -33,6 +42,15 @@ var vue_interestedAttorneys = new Vue({
 			this.showSelected = true;
 			this.selectedItem = attorney;
 			this.selectedId = id;
+
+			var that = this;
+			ifClientConnectedTo(this.query, id, function() {
+				// connected
+				that.selectedEnabled = false;
+			}, function() {
+				// not connected
+				that.selectedEnabled = true;
+			});
 		},
 
 		connect: function(attorneyId) {
