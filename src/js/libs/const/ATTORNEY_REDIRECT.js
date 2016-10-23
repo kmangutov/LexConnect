8a9440ea39808b8a71f8037488e8450d8de04eb6
Kirill Mangutov <kmangutov@gmail.com>
2016-10-23 14:12:46 -0700

    fix shit

--- src/js/libs/const/ATTORNEY_REDIRECT.js
+++
@@ -1,3 +0,0 @@
-var _REGISTER_NEXT_PAGE = "attorney-create-profile.html";
-var _LOGIN_NEXT_PAGE = "attorney-dashboard.html";
-var _USE_DB = "attorneys";
--- src/js/libs/const/CLIENT_REDIRECT.js
+++
@@ -1,3 +0,0 @@
-var _REGISTER_NEXT_PAGE = "client-create-profile.html";
-var _LOGIN_NEXT_PAGE = "client-dashboard.html";
-var _USE_DB = "clients";
