10de4dc13cdec0fa6ef568732360ec8c46bfbac2
Kirill Mangutov <kmangutov@gmail.com>
2016-12-15 23:46:35 -0800

    progress on questionnaire

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -10,3 +10,29 @@ var currentNode = QuestionTree[firstAnswer.nodeId];
 
 dump("firstAnswer", firstAnswer);
 dump("secondQuestion", currentNode);
+
+var vue = new Vue({
+	el: '#main',
+	data: {
+		questionString: "Is this drugs?",
+		answerMap: currentNode.options
+		 /*{
+			"Alcohol": 40,
+			"Drugs": 57
+		}*/
+	},
+	methods: {
+		selectOption(newNodeId, selectedText) {
+			if (newNodeId != -1) {
+				this.questionString = QuestionTree[newNodeId].value;
+				this.answerMap = QuestionTree[newNodeId].options;
+			} else {
+				this.nextPage();
+			}
+		},
+
+		nextPage() {
+			alert("nextPage");
+		}	
+	}
+});
