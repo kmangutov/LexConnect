70a2a361ea831499f3d8024ed68340c7b4ce642b
Kirill Mangutov <kmangutov@gmail.com>
2016-12-18 10:03:49 -0800

    add email sending and thanks page

--- js/email.js
+++ js/email.js
@@ -1,8 +1,27 @@
 
-var history = Lockr.get("history");
-var test = Lockr.get("FIRST_ANSWER");
-dump("test", test);
-dump("history", history);
-dump("fuck", Lockr.getAll());
 
-dump("last try", Lockr.getAll()[0]);
+console.log("wtf " + JSON.stringify(Lockr.get("history")));
+
+var html = "<table>";
+Lockr.get("history").forEach(function(row) {
+	html += "<tr><td>" + row.question + "</td><td>" + row.answer + "</td></tr>";
+});
+html += "</table>";
+
+
+$('#submit').click(function() {
+
+	$('#submit').text("Working...");
+	$('#submit').prop('disabled', true);
+
+	var obj = {
+		zip: $('#zipcode').val(),
+		email: $('#email').val(), 
+		html: html
+	}
+	emailjs.send("gmail", "form_submission", obj)
+		.then(function(response) {
+			window.location.href = "thanks.html";
+		});
+
+});
