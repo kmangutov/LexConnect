0a393865c8acc6dc1bc54ea3af586f86473b9ee4
Kirill Mangutov <kmangutov@gmail.com>
2017-02-20 13:16:17 -0800

    add ctoa2

--- assets/custom/js/index.js
+++ assets/custom/js/index.js
@@ -2,6 +2,7 @@
 Lockr.flush();
 var callToActionSubmit = $('#ctoa');
 var callToActionDropdown = $('#index-select');
+var ctoa2 = $('#ctoa2');
 
 callToActionSubmit.click(function() {
 	var selected = callToActionDropdown.children("option").filter(":selected");
@@ -10,6 +11,16 @@ callToActionSubmit.click(function() {
 	var struct = {
 		text: text,
 		nodeId: value
+	};
+	Lockr.set('FIRST_QUESTION', "What is your issue?");
+	Lockr.set('FIRST_ANSWER', struct);
+	window.location.href = "questionnaire.html";
+});
+
+ctoa2.click(function() {
+	var struct = {
+		text: "Unsure of issue",
+		nodeId: 123
 	};
 	Lockr.set('FIRST_QUESTION', "What is your issue?");
 	Lockr.set('FIRST_ANSWER', struct);
