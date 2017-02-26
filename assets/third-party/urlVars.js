9f45bb0b5ae28f5583a7cbcd8ae58f65ccd67829
Kirill Mangutov <kmangutov@gmail.com>
2017-02-26 15:55:51 -0800

    merge referee side

---
+++ assets/third-party/urlVars.js
@@ -0,0 +1,23 @@
+
+// eg  var type = getUrlVars()["type"], get url parameters
+var getUrlVars = function() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+
+        if($.inArray(hash[0], vars)>-1)
+        {
+            vars[hash[0]]+=","+hash[1];
+        }
+        else
+        {
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+    }
+
+    return vars;
+}
+
