d1a0e28389c88ea5dc02d009afd5629af7d01ab5
Kirill Mangutov <kmangutov@gmail.com>
2017-06-07 23:10:06 -0700

    create dui landing page leading to dui questionnaire

--- assets/custom/js/dui.js
+++ assets/custom/js/dui.js
@@ -1,19 +1,30 @@
+
+
+var countyVal = getUrlVars()['c'];
+var source = getUrlVars()['s'];
+
+
+alert("CountyVal: " + countyVal + ", source: " + source);
+
+
+var DEFAULT_STEP = 2;
+
 var vue = new Vue({
   el: '#description-row',
   data: {
-    step: 0,
+    step: DEFAULT_STEP,
     lastStep: 7,
   },
   methods: {
     back() {
-      if (this.step > 0) {
+      if (this.step > DEFAULT_STEP) {
         this.step--;
       }
     },
 
     next() {
       if (this.step == this.lastStep) {
-        window.location.href = "email.html";
+        window.location.href = 'email.html';
         this.step = -1;
         return;
       }
